6d663a484844dc74172fe3025d98aa021a21d45f
Brydon Holsey <brydon.holsey@ufl.edu>
2020-03-01 21:12:42 -0500

    Add javascript to enable dropdowns

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
 // It is always helpful to use comments in your code!
+
+var navLinks = document.getElementsByClassName('nav-link');
+var dropdown = document.getElementsByClassName('sub-menu')
+
+function toggleDropdown() {
+    // for (i = 0; i < navLinks.length; i++) {
+    //     dropdown[i].classList.toggle('show');
+    // }
+    this.children[1].classList.toggle('show');
+    
+}
+
+for (i = 0; i < navLinks.length; i++) {
+    navLinks[i].addEventListener('mouseenter', toggleDropdown);
+    navLinks[i].addEventListener('mouseleave', toggleDropdown);
+}
